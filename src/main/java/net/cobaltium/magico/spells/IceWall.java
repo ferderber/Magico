e90ae28ebb51f6d3a331a3a8ac7b05290bd9dcd9
mferds <matthewferderber@gmail.com>
2017-08-17 23:29:15 -0400

    in progress IceWall refactor

--- src/main/java/net/cobaltium/magico/spells/IceWall.java
+++ src/main/java/net/cobaltium/magico/spells/IceWall.java
@@ -8,16 +8,26 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.util.blockray.BlockRay;
 import org.spongepowered.api.util.blockray.BlockRayHit;
 import org.spongepowered.api.world.BlockChangeFlag;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.concurrent.TimeUnit;
 
 public class IceWall implements Spell {
+
+    private int wallSize;
+
+    public IceWall() {
+        wallSize = 3;
+    }
+
     public void handle(PluginContainer plugin, Player player) {
         BlockRay<World> blockRay = BlockRay.from(player).stopFilter(BlockRay.onlyAirFilter()).distanceLimit(3).build();
         Optional<BlockRayHit<World>> hit_ = blockRay.end();
@@ -25,79 +35,68 @@ public class IceWall implements Spell {
             BlockRayHit<World> hit = hit_.get();
             Location<World> location = hit.getLocation();
             hit.getFaces()[0].getOpposite();
-            Vector3d vec = hit.getDirection().cross(0, 1, 0);
-            Location<World>[][] wall = getWall(location, findNearest(vec));
+            Vector3d perpLookVector = hit.getDirection().cross(0, 1, 0); //get wall direction vector
+            Vector3i wallVector = Direction.getClosest(perpLookVector, Direction.Division.CARDINAL).asBlockOffset();
+            Location<World>[][] wall = getWall(location, wallVector);
 
-            BlockSnapshot[][] wallSnapshot = getWallSnapshot(wall);
+            Queue<BlockSnapshot> wallSnapshot = getWallSnapshot(wall);
             for (int i = 0; i < wall.length; i++) {
                 for (int j = 0; j < wall[i].length; j++) {
-                    wall[i][j].setBlockType(BlockTypes.PACKED_ICE, Cause.source(plugin).build());
+                    if(wall[i][j] != null) {
+                        wall[i][j].setBlockType(BlockTypes.PACKED_ICE, BlockChangeFlag.NONE, Cause.source(plugin).build());
+                    }
                 }
             }
 
             Task.builder().delay(2, TimeUnit.SECONDS).execute(() -> {
-                for (int i = 0; i < wall.length; i++) {
-                    for (int j = 0; j < wall[i].length; j++) {
-                        wall[i][j].restoreSnapshot(wallSnapshot[i][j], true, BlockChangeFlag.ALL, Cause.source(plugin).build());
-                    }
+                while(!wallSnapshot.isEmpty()) {
+                    wallSnapshot.remove().restore(true, BlockChangeFlag.NONE);
                 }
             }).submit(plugin);
         }
     }
 
-    public Vector3i findNearest(Vector3d vec) {
-        Vector3d vec2 = vec.abs();
-        if (vec2.getX() > vec2.getY()) {
-            if (vec2.getX() > vec2.getZ()) {
-                if (vec.getX() > 0) {
-                    return new Vector3i(1, 0, 0);
-                } else {
-                    return new Vector3i(-1, 0, 0);
-                }
-            } else {
-                if (vec.getZ() > 0) {
-                    return new Vector3i(0, 0, 1);
-                } else {
-                    return new Vector3i(0, 0, -1);
-                }
-            }
-        }
-        if (vec2.getY() > vec2.getZ()) {
-            if (vec.getY() > 0) {
-                return new Vector3i(0, 1, 0);
-            } else {
-                return new Vector3i(0, -1, 0);
-            }
-        } else {
-            if (vec.getZ() > 0) {
-                return new Vector3i(0, 0, 1);
-            } else {
-                return new Vector3i(0, 0, -1);
-            }
-        }
-    }
-
     public Location<World>[][] getWall(Location<World> center, Vector3i vec) {
-        vec = vec.add(0, 1, 0);
-        Location<World>[][] wall = new Location[3][3];
-        wall[1][1] = center;
-        wall[0][0] = center.add(vec.mul(-1, 1, -1));
-        wall[0][1] = wall[0][0].add(vec.mul(1, 0, 1));
-        wall[0][2] = wall[0][1].add(vec.mul(1, 0, 1));
-        wall[1][0] = center.add(vec.mul(-1, 0, -1));
-        wall[1][2] = center.add(vec.mul(1, 0, 1));
-        wall[2][0] = wall[1][0].add(vec.mul(0, -1, 0));
-        wall[2][1] = wall[2][0].add(vec.mul(1, 0, 1));
-        wall[2][2] = wall[2][1].add(vec.mul(1, 0, 1));
+        Location<World>[][] wall = new Location[wallSize][wallSize];
+        int middle = wallSize / 2;
+        if(wallSize % 2 == 0) {
+            //two middles hopefully we don't use this
+        } else {
+            if(vec.getX() == 0 && vec.getY() == 1 && vec.getZ() == 0) {
+                vec = vec.add(1, 0, 1);
+                wall[0][0] = center.add(vec.mul(-middle, middle, -middle)); //set first position by offset from center
+                for (int i = 0; i < wall.length; i++) {
+                    for (int j = 0; j < wall.length; j++) {
+                        wall[i][j] = wall[0][0].add(vec.mul(i, 0, -j));
+                        if (wall[i][j].getBlockType() != BlockTypes.AIR) {
+                            wall[i][j] = null; //Only give location of blocks that are safe to replace
+                        }
+                    }
+                }
+            }
+            else {
+                vec = vec.add(0, 1, 0);
+                wall[0][0] = center.add(vec.mul(-middle, middle, -middle)); //set first position by offset from center
+                for (int i = 0; i < wall.length; i++) {
+                    for (int j = 0; j < wall.length; j++) {
+                        wall[i][j] = wall[0][0].add(vec.mul(i, -j, i));
+                        if (wall[i][j].getBlockType() != BlockTypes.AIR) {
+                            wall[i][j] = null; //Only give location of blocks that are safe to replace
+                        }
+                    }
+                }
+            }
+        }
         return wall;
     }
 
 
-    public BlockSnapshot[][] getWallSnapshot(Location<World>[][] wall) {
-        BlockSnapshot[][] wallSnapshot = new BlockSnapshot[3][3];
+    public Queue<BlockSnapshot> getWallSnapshot(Location<World>[][] wall) {
+        Queue<BlockSnapshot> wallSnapshot = new LinkedList();
         for (int i = 0; i < wall.length; i++) {
-            for (int j = 0; j < wall[i].length; j++) {
-                wallSnapshot[i][j] = wall[i][j].createSnapshot();
+            for (int j = 0; j < wall[i].length; j++){
+                if(wall[i][j] != null)
+                    wallSnapshot.add(wall[i][j].createSnapshot());
             }
         }
         return wallSnapshot;
